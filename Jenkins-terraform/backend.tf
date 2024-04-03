30e792d7ad5574c5a70d3c804ad845f0f7d3d30a
ravitheja <ravithejareddy8008@gmail.com>
2024-04-03 18:09:39 +0530

    commit

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tetris-v17" # Replace with your actual S3 bucket name
+    bucket = "bucket-for-tetris-project" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
   }
